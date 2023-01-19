ae9b66b103ea55856bccf93fd6032c069bf071f6
Alba <albajimenezmnoz@gmail.com>
2023-01-19 08:30:43 +0100

    Day 1 & 2 completed

---
+++ day_1/helloword.py
@@ -0,0 +1,34 @@
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+'Alba'
+'Jimenez'
+'España'
+'I am enjoying 30 days of python'
+type(10)
+type(9.8)
+type(3.14)
+type(4-4j)
+type(['Asabeneh', 'Python', 'Fidland'])
+type('Alba')
+type('Jimenez')
+type('España')
+print(9)
+print(9.5)
+print(7+j)
+print('It is christmas time')
+True # Is the table black? If it is black, then the value is True
+False # Is the table black? If it is not black, then the value is False
+['red', 'blue', 'white', 'yellow'] 
+('Alba', 'Guillermo', 'Elena', 'Carolina')
+{4, 8, 16, 32}
+{
+    'first_name':'Alba',
+    'last_name':'Jimenez',
+    'country':'España',
+    'age':'16',
+}
---
+++ day_2/variables.py
@@ -0,0 +1,63 @@
+#Day 2. 30 Days of python programming
+first_name = 'Alba'
+last_name = 'Jimenez'
+full_name = 'Alba Jimenez'
+country_name= 'España'
+city_variable = 'Jerez'
+age_variable = '16'
+year_variable = '2023'
+is_married =False
+is_true =True
+is_light_on =True
+'Alba Jimenez España Jerez 16 2023 False True True'
+type('Alba')
+type('Jimenez')
+type('Alba Jimenez') 
+type('España')
+type('Jerez')
+type('16')
+type('2023')
+len(first_name)
+print(len(full_name))
+num_one=5
+print(num_one)
+num_two=4
+print(num_two)
+total = num_one + num_two
+print(total)
+diff = num_one - num_two
+print(diff)
+product = num_one * num_two
+print(product)
+division = num_one / num_two
+print(division)
+floor_division = num_one // num_two
+print(floor_division)
+reminder = num_two % num_one
+r = 30
+pi = 3.14
+diameter = r * 2
+area_of_circle = pi * r
+circum_of_circle = pi * diameter
+print(area_of_circle , circum_of_circle)
+
+r = input("¿cual es el radio?")
+print(f"el radio es {r}")
+
+area_of_circle = pi * r
+circum_of_circle = pi * diameter
+
+first_name = input("¿cual es tu nombre?")
+print(f"mi nombre es {first_name}")
+last_name = input("¿cual es tu apellido?")
+print(f"mi apellido es {last_name}")
+full_name = input("¿cual es tu nombre completo")
+print(f"mi nombre completo es {full_name}")
+country_name = input("¿en que pais vives?")
+print(f"vivo en {country_name}")
+city_variable = input("¿en que ciudad vives?")
+print(f"vivo en {city_variable}")
+age_variable = input("¿cuantos años tienes?")
+print(f"tengo {age_variable} años")
+
+help("keywords")
