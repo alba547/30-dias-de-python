d1b38c1e0119587673395e49bdfb33a44dc29dd1
Alba <albajimenezmnoz@gmail.com>
2023-02-17 11:10:23 +0100

    dia_4_nocompletado

---
+++ day_4/strings.py
@@ -0,0 +1,42 @@
+space = ""
+print(space)
+print(len(space))
+word1 = 'thirty'
+print(word1)
+print(len(word1))
+word2='days'
+print(word2)
+print(len(word2))
+word3='of'
+print(word3)
+print(len(word3))
+word4='python'
+print(word4)
+print(len(word4))
+sentence= space + word1 + word2 + word3 + word4
+print(sentence)
+print(len(sentence))
+
+space=""
+word5 = 'coding'
+print(word5)
+print(len(word5))
+word6 = 'for'
+print(word6)
+print(len(word6))
+word7 = 'all'
+print(word7)
+print(len(word7))
+sentence2 = space + word5 + word6 + word7
+print(sentence2)
+print(len(sentence2))
+
+company = sentence2
+
+print(company)
+print(len(company))
+
+company.upper
+
+company.lower
+
