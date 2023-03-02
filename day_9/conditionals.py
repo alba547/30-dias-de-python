0b6f33318a20705851cdcfdfd96054f8080cdb6e
Alba <albajimenezmnoz@gmail.com>
2023-03-02 09:15:49 +0100

    dia 9

---
+++ day_9/conditionals.py
@@ -0,0 +1,20 @@
+age = input(30)
+if age > 18:
+    print ('You are old enough to learn to drive.')
+age = input(15)
+if age < 18:
+    print ('You need 3 more years to learn to drive.')  #1
+
+my_age = input(16)
+your_age = input(30)
+print(f'I am{your_age - my_age} older than you') if your_age > my_age else print (f'You are {my_age - your_age} older than me') #2
+
+a = 4
+b = 3
+if a > b : 
+    print('a is greater than b')
+elif a < b :
+        print ('a is smaller than b')
+else :
+    print('a is equal to b') #3
+
