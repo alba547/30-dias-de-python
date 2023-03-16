5101a70d8e637132492d1162f00b084ac9b83887
Alba <albajimenezmnoz@gmail.com>
2023-03-16 09:03:03 +0100

    changes

---
+++ day_10/loops.py
@@ -0,0 +1,14 @@
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for number in numbers:
+    print(number)
+count = 0
+while count <=10:
+    print(count)
+    count = count + 1
+
+for number in numbers:
+    print(numbers.reverse)
+count2 = 10
+while count2 >= 0:
+    print(count2)
+    count2 = count2 - 1
